912744c3dcf9ab6aaf8b2aa9398775d6b7d91173
Kalash Jain <na19b040@smail.iitm.ac.in>
2021-03-30 10:38:00 +0530

    NewComplaint Screen Updated

--- lib/screens/NewComplaint/main.dart
+++ lib/screens/NewComplaint/main.dart
@@ -1,4 +1,9 @@
+
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
 class NewComplaintScreen extends StatefulWidget {
   @override
@@ -9,13 +14,19 @@ class _NewComplaintScreenState extends State<NewComplaintScreen> {
 
   TextEditingController compLocation = TextEditingController();
   TextEditingController compDescription = TextEditingController();
+  String dropdownValue = 'Type of Waste';
+  List<Asset> images = [];
+  String error;
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
+        SizedBox(
+          height: 20,
+        ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: TextField(
             decoration: InputDecoration(
               labelText: 'Description',
@@ -23,6 +34,9 @@ class _NewComplaintScreenState extends State<NewComplaintScreen> {
             maxLines: null,
             controller: compDescription,
           ),
+        ),
+        SizedBox(
+          height: 20,
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
@@ -38,17 +52,59 @@ class _NewComplaintScreenState extends State<NewComplaintScreen> {
             maxLines: null,
           ),
         ),
-        // SizedBox(
-        //   height: 40,
-        // ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: DropdownButton(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_drop_down),
+            iconSize: 24,
+            elevation: 16,
+            isExpanded: true,
+            style: const TextStyle(color: Colors.deepPurple),
+              underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String newValue) {
+              setState(() {
+               dropdownValue = newValue;
+              });
+            },
+            items: <String>['Type of Waste', 'Bio-degradable', 'Plastic'].map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                child: Text(value),
+                value: value,
+              );
+            }).toList(),
+          ),
+        ),
+        SizedBox(
+        height: 40,
+        ),
+        if (images.length != 0) dispImages(),
         SizedBox(
           height: 40,
         ),
-        Center(
-          child: ElevatedButton(
-            child: Text('Add Images'),
-            onPressed: null,
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Add Images'),
+              onPressed: loadAssets,
+            ),
+            if(images.length != 0)SizedBox(width: 10,),
+            if(images.length != 0)ElevatedButton(
+              child: Text('Clear'),
+              onPressed: (){
+                setState(() {
+                  images.removeRange(0, images.length);
+                });
+              },
+            ),
+          ]
         ),
         SizedBox(
           height: 40,
@@ -61,5 +117,80 @@ class _NewComplaintScreenState extends State<NewComplaintScreen> {
         ),
       ],
     );
+  }
+
+  Widget dispImages() {
+    return Container(
+      height: 200,
+      child: ListView.builder(
+        itemCount: images.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          Asset image = images[index];
+          return Card(
+            child: AssetThumb(
+              asset: image,
+              width: 300,
+              height: 300,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Future<void> loadAssets() async {
+    List<Asset> resultList = [];
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 300,
+        enableCamera: true,
+        selectedAssets: images,
+        cupertinoOptions: CupertinoOptions(
+          selectionFillColor: "#ff11ab",
+          selectionTextColor: "#ffffff",
+          selectionCharacter: "✓",
+        ),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#00BFFF",
+          actionBarTitle: "Select Images",
+          allViewTitle: "All Media",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+          lightStatusBar: true,
+          statusBarColor: "#4169E1",
+        ),
+      );
+    } on Exception catch (e) {
+      error = e.toString();
+      print(error);
+      Fluttertoast.showToast(
+          msg: error,
+          toastLength: Toast.LENGTH_SHORT,
+          backgroundColor: Colors.white,
+          textColor: Colors.black,
+          fontSize: 14.0
+      );
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      if(resultList.length==0){
+        print('No image selected');
+        Fluttertoast.showToast(
+            msg: 'No Image Selected',
+            toastLength: Toast.LENGTH_SHORT,
+            backgroundColor: Colors.white,
+            textColor: Colors.black,
+            fontSize: 14.0);
+      }else{
+        images = resultList;
+      }
+    });
   }
 }
