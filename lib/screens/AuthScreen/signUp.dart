f0345d48c1cc2a8549e0eb98fe06199f0ef07f35
V Vamsi Krishna Varma <vamsivarma02102001@gmail.com>
2021-06-23 04:32:27 +0000

    Update signUp.dart

--- lib/screens/AuthScreen/signUp.dart
+++ lib/screens/AuthScreen/signUp.dart
@@ -14,8 +14,11 @@ class _SignUpState extends State<SignUp> {
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
   final TextEditingController _name = TextEditingController();
+  final TextEditingController _confirmpass = TextEditingController();
   String error;
   bool loading = false;
+  bool _obscureText1 = true;
+  bool _obscureText2 = true;
   final _formKey = GlobalKey<FormState>();
   _signUp(auth) async {
     setState(() {
@@ -58,89 +61,149 @@ class _SignUpState extends State<SignUp> {
             ),
           ],
         ),
-        body: loading? Center(child: CircularProgressIndicator(),): Container(
-            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-            child: SingleChildScrollView(
-              child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 10.0),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.person),
-                            hintText: 'Name',
-                            //helperText: 'Enter your Name',
-                            border: OutlineInputBorder(),
+        body: loading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                child: SingleChildScrollView(
+                  child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 10.0),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.person),
+                                hintText: 'Name',
+                                //helperText: 'Enter your Name',
+                                border: OutlineInputBorder(),
+                              ),
+                              maxLines: null,
+                              controller: _name,
+                              validator: (val) =>
+                                  val.isEmpty ? 'Please enter your name' : null,
+                            ),
                           ),
-                          maxLines: null,
-                          controller: _name,
-                          validator: (val) =>
-                              val.isEmpty ? 'Please enter your name' : null,
-                        ),
-                      ),
 
-                      SizedBox(height: 15.0),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.email),
-                            hintText: 'Email',
-                            //helperText: 'Enter your email',
-                            border: OutlineInputBorder(),
+                          SizedBox(height: 15.0),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.email),
+                                hintText: 'Email',
+                                //helperText: 'Enter your email',
+                                border: OutlineInputBorder(),
+                              ),
+                              maxLines: null,
+                              controller: _email,
+                              validator: (val) => val.isEmpty
+                                  ? 'Please enter your email'
+                                  : null,
+                            ),
                           ),
-                          maxLines: null,
-                          controller: _email,
-                          validator: (val) =>
-                              val.isEmpty ? 'Please enter your email' : null,
-                        ),
-                      ),
 
-                      SizedBox(height: 15.0),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.create_rounded),
-                            hintText: 'Create Password',
-                            //helperText: 'Create a password',
-                            border: OutlineInputBorder(),
+                          SizedBox(height: 15.0),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  icon: Icon(Icons.create_rounded),
+                                  hintText: 'Create Password',
+                                  //helperText: 'Create a password',
+                                  border: OutlineInputBorder(),
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                      _obscureText1
+                                          ? Icons.visibility
+                                          : Icons.visibility_off,
+                                      color: Colors.grey[600],
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        _obscureText1 = !_obscureText1;
+                                      });
+                                    },
+                                  )),
+                              controller: _password,
+                              validator: (val) => val.length < 7
+                                  ? 'Enter a password 8+ chars long'
+                                  : null,
+                              obscureText: _obscureText1,
+                            ),
                           ),
-                          controller: _password,
-                          validator: (val) => val.length < 7
-                              ? 'Enter a password 8+ chars long'
-                              : null,
-                          obscureText: true,
-                        ),
-                      ),
-                      SizedBox(height: 15.0),
-                      // ignore: deprecated_member_use
-                      RaisedButton(
-                        color: Colors.blue,
-                        child: Text(
-                          'Register',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            await _signUp(auth);
-                          }
-                        },
-                      ),
-                      SizedBox(height: 6.0),
-                      error != null
-                          ? Text(
-                              error,
-                              style: TextStyle(color: Colors.red),
-                            )
-                          : SizedBox()
-                    ],
-                  )),
-            )),
+                          SizedBox(height: 15.0),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  icon: Icon(Icons.create_rounded),
+                                  hintText: 'Confirm Password',
+                                  //helperText: 'Confirm password',
+                                  border: OutlineInputBorder(),
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                      _obscureText2
+                                          ? Icons.visibility
+                                          : Icons.visibility_off,
+                                      color: Colors.grey[600],
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        _obscureText2 = !_obscureText2;
+                                      });
+                                    },
+                                  )),
+                              controller: _confirmpass,
+                              validator: (val) {
+                                if (val.isEmpty) {
+                                  return "Please Re-Enter New Password";
+                                } else if (val.length < 8) {
+                                  return "Password must be atleast 8 characters long";
+                                } else if (val != _password.text) {
+                                  return "Password must be same as above";
+                                } else {
+                                  return null;
+                                }
+                              },
+                              obscureText: _obscureText2,
+                            ),
+                          ),
+                          SizedBox(height: 15.0),
+
+                          // ignore: deprecated_member_use
+                          RaisedButton(
+                            color: Colors.blue,
+                            child: Text(
+                              'Register',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () async {
+                              if (_formKey.currentState.validate()) {
+                                await _signUp(auth);
+                              }
+                            },
+                          ),
+                          SizedBox(height: 6.0),
+                          error != null
+                              ? Text(
+                                  error,
+                                  style: TextStyle(color: Colors.red),
+                                )
+                              : SizedBox()
+                        ],
+                      )),
+                )),
       ),
     );
   }
 }
+
