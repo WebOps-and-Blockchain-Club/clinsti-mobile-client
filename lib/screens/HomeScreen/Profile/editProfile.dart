349f8f9a63d27e9a4affca320d8acaa4f9e8f762
JanithMS <janithms9920@gmail.com>
2021-06-30 13:17:37 +0530

    editProfile/changePass updated

--- lib/screens/HomeScreen/Profile/editProfile.dart
+++ lib/screens/HomeScreen/Profile/editProfile.dart
@@ -1,5 +1,6 @@
 import 'package:app_client/models/user.dart';
 import 'package:app_client/services/auth.dart';
+import 'package:app_client/widgets/formErrorMessage.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -17,6 +18,9 @@ class _MyEditProfileScreenState extends State<MyEditProfileScreen> {
   final _formKey = GlobalKey<FormState>();
   String error;
   bool loading=false;
+  String nameerror;
+  String emailerror;
+
   FocusNode nameFocusNode;
 
   @override
@@ -72,96 +76,144 @@ class _MyEditProfileScreenState extends State<MyEditProfileScreen> {
           Center(
             child: CircularProgressIndicator(),
           ) :
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+          Container(
+            color: Colors.white,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                BackButton(),
+                Container(
+                  width: double.infinity,
+                  height: 120,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: BackButton(),
+                ),
                 Expanded(
-                  child: SingleChildScrollView(
-                    child: Form(
-                      key: _formKey,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            "assets/user-01.png",
-                            scale: 0.8,
-                            color: Colors.green,
-                            ),
-                          SizedBox(height: 20),
-                          Material(
-                            elevation: 20.0,
-                            shadowColor: Colors.white,
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                hintText: "Name",
-                                prefixIcon: Icon(
-                                  Icons.person,
-                                  color: Colors.green,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: SingleChildScrollView(
+                      child: Form(
+                        key: _formKey,
+                        child: Container(
+                          color: Colors.white,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                "assets/user-01.png",
+                                scale: 1,
+                                color: Colors.green,
+                                ),
+                              SizedBox(height: 20),
+                              Material(
+                                elevation: 20.0,
+                                shadowColor: Colors.white,
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    errorStyle: TextStyle(height: 0),
+                                    hintText: "Name",
+                                    prefixIcon: Icon(
+                                      Icons.person,
+                                      color: Colors.green,
+                                      ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                      borderSide: BorderSide(color: Colors.white,width: 0.0),
+                                      ),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(Radius.circular(10.0))
+                                    )
                                   ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                  borderSide: BorderSide(color: Colors.white,width: 0.0),
+                                  readOnly: false,
+                                  controller: _name,
+                                  maxLines: null,
+                                  validator: (val) {
+                                    if (val.isEmpty) {
+                                      setState(() {
+                                        nameerror="Please Enter Name";
+                                      });
+                                      return null;
+                                    } else {
+                                      setState(() {
+                                        nameerror = null;
+                                      });
+                                      return null;
+                                      }
+                                  },
+                                ),
+                              ),
+                              errorMessages(nameerror),
+                              SizedBox(height: 20),
+                              Material(
+                                elevation: 20.0,
+                                shadowColor: Colors.white,
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    hintText: "Email",
+                                    prefixIcon: Icon(
+                                      Icons.email,
+                                      color: Colors.green,
+                                      ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                      borderSide: BorderSide(color: Colors.white,width: 0.0),
+                                      ),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(Radius.circular(10.0))
+                                    )
                                   ),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(10.0))
+                                  readOnly: false,
+                                  controller: _email,
+                                  maxLines: null,
+                                  validator: (val) {
+                                    if (val.isEmpty) {
+                                      setState(() {
+                                        emailerror="Please Enter Email";
+                                      });
+                                      return null;
+                                    } else {
+                                      setState(() {
+                                        emailerror = null;
+                                      });
+                                      return null;
+                                    }
+                                  },
+                                ),
+                              ),
+                              errorMessages(emailerror),
+                              SizedBox(height: 20),
+                              Center(
+                                child: ElevatedButton.icon(
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all( Colors.green),
+                                    elevation: MaterialStateProperty.all( 10 )
+                                  ),
+                                  onPressed: ()async {
+                                    if(_formKey.currentState.validate())
+                                      await _updateUserProfile(
+                                        auth: widget.auth,
+                                        name: _name.text,
+                                        email: _email.text);},
+                                  icon: Icon(MdiIcons.circleEditOutline),
+                                  label: Text("Update Profile",textScaleFactor: 1.2,)),
+                              ),
+                              SizedBox(height: 20,),
+                              error != null ?
+                                Text(
+                                  error,
+                                  style: TextStyle(color: Colors.red),
                                 )
-                              ),
-                              readOnly: false,
-                              controller: _name,
-                              maxLines: null,
-                              validator: (val) => val.isEmpty ? "Please enter your name" : null,
-                            ),
+                                : SizedBox()
+                            ],
                           ),
-                          SizedBox(height: 20),
-                          Material(
-                            elevation: 20.0,
-                            shadowColor: Colors.white,
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10.0),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                hintText: "Email",
-                                prefixIcon: Icon(
-                                  Icons.email,
-                                  color: Colors.green,
-                                  ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                  borderSide: BorderSide(color: Colors.white,width: 0.0),
-                                  ),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(10.0))
-                                )
-                              ),
-                              readOnly: false,
-                              controller: _email,
-                              maxLines: null,
-                              validator: (val) => val.isEmpty ? "Please enter your email" : null,
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                          Center(
-                            child: ElevatedButton.icon(
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all( Colors.green),
-                                elevation: MaterialStateProperty.all( 10 )
-                              ),
-                              onPressed: ()async {
-                                if(_formKey.currentState.validate())
-                                  await _updateUserProfile(
-                                    auth: widget.auth,
-                                    name: _name.text,
-                                    email: _email.text);},
-                              icon: Icon(MdiIcons.circleEditOutline),
-                              label: Text("Update Profile")),
-                          ),
-                        ],
-                      )
+                        )
+                      ),
                     ),
                   ),
                 ),
